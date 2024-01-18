2e8bb1dc7e08e7d890d3456f43528a5fd50e635c
Amber Kortier <AmberKortier@gmail.com>
2024-01-18 12:54:19 +0100

    Sample Mod

---
+++ BowloutSampleMod/BowloutSampleMod.cs
@@ -0,0 +1,53 @@
+﻿using BowloutModManager.BowloutMod;
+using BowloutModManager.BowloutMod.Interfaces;
+using System;
+
+namespace BowloutSampleMod
+{
+    public class BowloutSampleMod : IBowloutMod
+    {
+        public string Name => "Bowlout Sample Mod";
+
+        public Version Version => new Version(1, 0, 0, 0);
+
+        public string Description => "A sample mod for Bowlout.";
+
+        public IBowloutConfiguration Configuration { get; private set; }
+        public SampleModConfiguration SampleModConfiguration => (SampleModConfiguration)Configuration;
+
+        public void OnSetup()
+        {
+            Configuration = this.GetConfiguration<SampleModConfiguration>() ?? new SampleModConfiguration();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public void OnEnable()
+        {
+
+        }
+
+        public void OnDisable()
+        {
+            
+        }
+
+        public void OnUpdate()
+        {
+
+        }
+
+        public void OnFixedUpdate()
+        {
+            
+        }
+
+        public void OnLateUpdate()
+        {
+            
+        }
+    }
+}
---
+++ BowloutSampleMod/SampleModConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using BowloutModManager.BowloutMod.Interfaces;
+using UnityEngine;
+
+namespace BowloutSampleMod
+{
+    public class SampleModConfiguration : IBowloutConfiguration
+    {
+        public int Version { get => 1; set => _ = value; }
+
+        public bool IAmABoolConfig;
+
+        // The range attribute is MANDATORY
+        [Range(0, 10)]
+        public float IAmAFloatConfig;
+
+        // The range attribute is MANDATORY
+        [Range(0, 10)]
+        public int IAmAIntConfig;
+    }
+}
